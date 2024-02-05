ad677d3f71311c43e6f96c2aafb825e6432dece9
joão Victor <114033813+victorJvss@users.noreply.github.com>
2024-02-05 14:43:35 -0300

    Implementação da biblíoteca swagger

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { useContainer } from 'class-validator';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,15 @@ async function bootstrap() {
       forbidNonWhitelisted: true
     })
   )
+  const config = new DocumentBuilder()
+  .setTitle('Clientes')
+  .setDescription('Requisição HTTP na rota /usuario e na rota /enderecos')
+  .setVersion('1.0')
+  .addTag('Cliente')
+  .build();
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 
   useContainer(app.select(AppModule), {fallbackOnErrors: true});

6ff1f2d76a2a5b0deca79df4de566fc5e7e19601
joão Victor <114033813+victorJvss@users.noreply.github.com>
2024-02-05 14:46:59 -0300

    Adaptando as opções de menssagem do maxLength

--- src/usuario/usuarioDto/EnderecoCliente.dto.ts
+++ src/usuario/usuarioDto/EnderecoCliente.dto.ts
@@ -2,19 +2,19 @@ import { IsNotEmpty, MaxLength } from 'class-validator';
 
 export class EnderecoDto {
   @IsNotEmpty({ message: 'O campo estado não pode ser vazio!' })
-  @MaxLength(35)
+  @MaxLength(35, { message: 'O campo deve ter no máximo 53 caracteres' })
   estado: string;
 
   @IsNotEmpty({ message: 'O campo cidade não pode ser vazio!' })
-  @MaxLength(35)
+  @MaxLength(35, { message: 'O campo deve ter no máximo 53 caracteres' })
   cidade: string;
 
   @IsNotEmpty({ message: 'O campo bairro não pode ser vazio!' })
-  @MaxLength(35)
+  @MaxLength(35, { message: 'O campo deve ter no máximo 53 caracteres' })
   bairro: string;
 
   @IsNotEmpty({ message: 'O campo rua não pode ser vazio!' })
-  @MaxLength(15)
+  @MaxLength(15, { message: 'O campo deve ter no máximo 15 caracteres' })
   rua: string;
 
   @IsNotEmpty({ message: 'O campo número da casa não pode ser vazio!' })

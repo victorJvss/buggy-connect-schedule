b2d6adbeb4d01e0ab4203042693e56c43d7eaf2f
joão Victor <114033813+victorJvss@users.noreply.github.com>
2024-02-07 12:10:17 -0300

    Estrutura Entity

---
+++ src/passeios/passeiosEntity/passeios.entity.ts
@@ -0,0 +1,8 @@
+export class PasseiosEntity {
+  datalhesPasseios: string;
+  horarios: string;
+  distancia: number;
+  tempoPasseio: number;
+  valor: number;
+  //   formasDePagamentos: formasDePagamento;
+}

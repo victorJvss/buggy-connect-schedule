8f030dc4b9b082ce32524d56ab8ab0ae030e921c
joão Victor <114033813+victorJvss@users.noreply.github.com>
2024-02-07 12:04:33 -0300

    injeção do service passeios

--- src/passeios/passeios.module.ts
+++ src/passeios/passeios.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { PasseiosService } from './passeios.service';
 
 @Module({
   controllers: [],
-  providers: [],
+  providers: [PasseiosService],
 })
 export class PasseiosModule {}

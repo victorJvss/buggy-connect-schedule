637a824ad9f51548de42be0ed1b8154729e2bfb9
joão Victor <114033813+victorJvss@users.noreply.github.com>
2024-02-08 14:18:55 -0300

    Criação do teste que deve criar um novo passeio

---
+++ src/passeios/test/passeios.service.spec.ts
@@ -0,0 +1,53 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { PasseiosService } from '../passeios.service';
+import { mockPasseio, mockPasseioService } from '../mocks/passeio.service.mock';
+
+describe('Teste passeio controller', () => {
+  let passeioService: PasseiosService;
+
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [mockPasseioService],
+    }).compile();
+
+    passeioService = module.get<PasseiosService>(PasseiosService);
+  });
+
+  it('Deve retornar todos os passeios', async () => {
+    const retornarPasseios = await passeioService.pegaTodosOsPasseios();
+    expect(retornarPasseios).toEqual([mockPasseio]);
+  });
+
+  it('Deve retornar o passeio pelo id', async () => {
+    const retornarPasseioPeloId = await passeioService.pegaPasseioPeloId(
+      mockPasseio.id,
+    );
+
+    expect(retornarPasseioPeloId.id).toBe('1');
+  });
+
+  it('Deve Criar um passeio', async () => {
+    const passeio = {
+      nomePasseio: 'PRAIA DE GENIPABU',
+
+      datalhesPasseios:
+        'Desfrute de um passeio tranquilo pelas famosas dunas de Jenipabu em nosso buggy confortável e seguro. Guiados por nossos motoristas experientes, você e sua família terão a oportunidade de explorar as maravilhas naturais desta região, enquanto deslizam suavemente pelas dunas e apreciam as vistas panorâmicas deslumbrantes. Aproveite para tirar fotos memoráveis e sentir a brisa suave do oceano acariciar seu rosto.',
+
+      horarios: '5:00 as 17:00',
+
+      distancia: '15KM',
+
+      tempoPasseio: '40 minutos',
+      valor: 'R$ 150',
+    };
+
+    const id = '2';
+
+    passeioService.adicionaPasseios = jest
+      .fn()
+      .mockResolvedValueOnce({ id, ...passeio });
+
+    const criaPasseio = await passeioService.adicionaPasseios(passeio);
+    expect(criaPasseio.id).toBe('2');
+  });
+});

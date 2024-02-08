e167a16bacb81d79280d4ffbb481241b9d07a8b5
joão Victor <114033813+victorJvss@users.noreply.github.com>
2024-02-08 13:59:24 -0300

    Criação do mock passeioService

---
+++ src/passeios/mocks/passeio.service.mock.ts
@@ -0,0 +1,26 @@
+import { PasseiosService } from '../passeios.service';
+
+export const mockPasseio = {
+  nomePasseio: 'PRAIA DE GENIPABU',
+
+  datalhesPasseios:
+    'Desfrute de um passeio tranquilo pelas famosas dunas de Jenipabu em nosso buggy confortável e seguro. Guiados por nossos motoristas experientes, você e sua família terão a oportunidade de explorar as maravilhas naturais desta região, enquanto deslizam suavemente pelas dunas e apreciam as vistas panorâmicas deslumbrantes. Aproveite para tirar fotos memoráveis e sentir a brisa suave do oceano acariciar seu rosto.',
+
+  horarios: '5:00 as 17:00',
+
+  distancia: '15KM',
+
+  tempoPasseio: '40 minutos',
+  valor: 'R$ 150',
+};
+
+export const mockPasseioService = {
+  provide: PasseiosService,
+  useValue: {
+    pegaTodosOsPasseios: jest.fn().mockResolvedValue([mockPasseio]),
+    pegaPasseioPeloId: jest.fn().mockResolvedValue(mockPasseio),
+    adicionaPasseios: jest.fn().mockResolvedValue(mockPasseio),
+    atualizaPasseios: jest.fn().mockResolvedValue(mockPasseio),
+    deletaPasseio: jest.fn().mockResolvedValue(mockPasseio),
+  },
+};

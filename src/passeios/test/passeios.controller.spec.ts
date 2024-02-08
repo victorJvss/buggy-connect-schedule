f14f0569ad181b715f7e15e236783e2c64fc337f
joão Victor <114033813+victorJvss@users.noreply.github.com>
2024-02-08 14:05:19 -0300

    Criação do teste que deve retornar todos os passeios

--- src/passeios/test/passeios.controller.spec.ts
+++ src/passeios/test/passeios.controller.spec.ts
@@ -1,15 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PasseiosService } from '../passeios.service';
+import { mockPasseio, mockPasseioService } from '../mocks/passeio.service.mock';
 
 describe('Teste passeio controller', () => {
   let passeioService: PasseiosService;
 
   beforeAll(async () => {
-    const module: TestingModule = Test.createTestingModule({
-      controllers: [],
-      providers: [],
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [mockPasseioService],
     }).compile();
 
     passeioService = module.get<PasseiosService>(PasseiosService);
+  });
+
+  it('Deve retornar todos os passeios', async () => {
+    const retornarPasseios = await passeioService.pegaTodosOsPasseios();
+    expect(retornarPasseios).toEqual([mockPasseio]);
   });
 });

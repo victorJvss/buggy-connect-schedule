5dd80bbc73233c1a2c1eae6572267f0ecda8e6de
joão Victor <114033813+victorJvss@users.noreply.github.com>
2024-02-15 14:50:32 -0300

    Criação do teste no passeio.controller que deve adicionar um passeio

--- src/passeios/test/passeio.controller.spec.ts
+++ src/passeios/test/passeio.controller.spec.ts
@@ -24,4 +24,30 @@ describe('Teste do  controller', () => {
 
     expect(pegaPasseioPeloId.id).toBe('1');
   });
+
+  it('Deve criar um passeio', async () => {
+    const passeio = {
+      nomePasseio: 'PRAIA DE GENIPABU',
+
+      datalhesPasseios:
+        'Desfrute de um passeio tranquilo pelas famosas dunas de Jenipabu em nosso buggy confortável e seguro. Guiados por nossos motoristas experientes, você e sua família terão a oportunidade de explorar as maravilhas naturais desta região, enquanto deslizam suavemente pelas dunas e apreciam as vistas panorâmicas deslumbrantes. Aproveite para tirar fotos memoráveis e sentir a brisa suave do oceano acariciar seu rosto.',
+
+      horarios: '5:00 as 17:00',
+
+      distancia: '15KM',
+
+      tempoPasseio: '40 minutos',
+      valor: 'R$ 150',
+    };
+
+    const id = '2';
+
+    passeioController.adicionaPasseio = jest
+      .fn()
+      .mockResolvedValueOnce({ id, ...passeio });
+
+    const adicionaPasseio = await passeioController.adicionaPasseio(passeio);
+
+    expect(adicionaPasseio.id).toBe('2');
+  });
 });

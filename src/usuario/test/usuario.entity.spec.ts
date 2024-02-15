d7ab2dfce2ea7ffaa304fda0a70358625ee0e36a
joão Victor <114033813+victorJvss@users.noreply.github.com>
2024-02-15 15:41:20 -0300

    Teste no entity dadosCnh

--- src/usuario/test/usuario.entity.spec.ts
+++ src/usuario/test/usuario.entity.spec.ts
@@ -1,10 +1,17 @@
 import { ClienteEntity } from '../usuarioEntity/Cliente.entity';
+import { DadosCnh } from '../usuarioEntity/DadosCnh.entity';
 
 describe('Deve testar o entity cliente', () => {
-  const usuarioEntity = new ClienteEntity();
+  const clienteEntity = new ClienteEntity();
+  const dadosCnh = new DadosCnh();
 
   it('Teste no entity cliente', () => {
-    usuarioEntity.nome = 'Raul';
-    expect(usuarioEntity).toEqual({ nome: 'Raul' });
+    clienteEntity.nome = 'Raul';
+    expect(clienteEntity).toEqual({ nome: 'Raul' });
+  });
+
+  it('Teste no entity dadoCnh', () => {
+    dadosCnh.numeroCnh = 56565674232;
+    expect(dadosCnh.numeroCnh).toBe(56565674232);
   });
 });

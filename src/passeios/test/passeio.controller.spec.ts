fde118e5e449913d037aeb40aaa8cb61f469736b
joão Victor <114033813+victorJvss@users.noreply.github.com>
2024-02-15 12:46:59 -0300

    Criação do teste GET que deve retornar o passeio pelo id

--- src/passeios/test/passeio.controller.spec.ts
+++ src/passeios/test/passeio.controller.spec.ts
@@ -2,18 +2,26 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { mockPasseio, mockPasseioService } from '../mocks/passeio.service.mock';
 import { PasseiosController } from '../passseios.controller';
 
-describe('Teste do passeio controller', () => {
+describe('Teste do  controller', () => {
   let passeioController: PasseiosController;
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PasseiosController],
       providers: [mockPasseioService],
     }).compile();
-    passeioController = module.get<PasseiosController>(PasseiosController)
+    passeioController = module.get<PasseiosController>(PasseiosController);
   });
 
   it('Deve retornar todos os passeios', async () => {
-    const pegaTodosOsPasseios = await passeioController.pegaTodosOsPasseios()
-    expect(pegaTodosOsPasseios).toEqual([mockPasseio])
-  })
+    const pegaTodosOsPasseios = await passeioController.pegaTodosOsPasseios();
+    expect(pegaTodosOsPasseios).toEqual([mockPasseio]);
+  });
+
+  it('Deve retornar o passeio pelo id', async () => {
+    const pegaPasseioPeloId = await passeioController.pegaPasseioPeloId(
+      mockPasseio.id,
+    );
+
+    expect(pegaPasseioPeloId.id).toBe('1');
+  });
 });

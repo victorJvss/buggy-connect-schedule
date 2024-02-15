2a268f3b58cf71cead6ca5911c756291f9786bc1
joão Victor <114033813+victorJvss@users.noreply.github.com>
2024-02-15 15:55:31 -0300

    Teste no entity ContaPessoal

--- src/usuario/test/usuario.entity.spec.ts
+++ src/usuario/test/usuario.entity.spec.ts
@@ -2,12 +2,14 @@ import { ClienteEntity } from '../usuarioEntity/Cliente.entity';
 import { DadosCnh } from '../usuarioEntity/DadosCnh.entity';
 import { MetodoRecebimento } from '../usuarioEntity/MetodoRecebimento.entity';
 import { TipoDeBuggy } from '../usuarioEntity/TipoDeCarro.entity';
+import { ContaPessoal } from '../usuarioEntity/TipoDeConta.entity';
 
 describe('Deve testar o entity cliente', () => {
   const clienteEntity = new ClienteEntity();
   const dadosCnh = new DadosCnh();
   const metodoRecebimento = new MetodoRecebimento();
   const tipoDeCarro = new TipoDeBuggy();
+  const contaPessoal = new ContaPessoal();
 
   it('Teste no entity cliente', () => {
     clienteEntity.nome = 'Raul';
@@ -27,5 +29,10 @@ describe('Deve testar o entity cliente', () => {
   it('Teste no entity TipoDeBuggy', () => {
     tipoDeCarro.corBuggy = 'Amarelo';
     expect(tipoDeCarro.corBuggy).toBe('Amarelo');
+  });
+
+  it('Teste no entity contaPessoal', () => {
+    contaPessoal.sexo = 'Masculino';
+    expect(contaPessoal.sexo).toBe('Masculino');
   });
 });

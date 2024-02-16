20036dcf559a076560a96ef798375279b3abb234
joão Victor <114033813+victorJvss@users.noreply.github.com>
2024-02-16 14:14:25 -0300

    Implementação do mock agendamentoService para os testes

---
+++ src/agendamentoPasseio/mocks/agendamento.service.mock.ts
@@ -0,0 +1,25 @@
+import { AgendamentoPasseioService } from '../agendamento.service';
+
+export const agendamentoMock = {
+  id: '1',
+  PasseioAgendado: 'PRAIA DE GENIPABU',
+  HorarioInícioPasseio: '15:00',
+  HorarioTerminoPasseio: '15:40',
+  QuantidadePessoas: 3,
+  ValorTotalPasseio: 'R$ 450',
+  ProfissionalPasseio: 'Roberto silva',
+  titularDoAgendamento: 'Davi ribeiro',
+};
+
+export const agendamentoMockService = {
+  provide: AgendamentoPasseioService,
+  useValue: {
+    retornarTodosOsPasseiosAgendados: jest
+      .fn()
+      .mockResolvedValue([agendamentoMock]),
+    retornaPasseioAgendadoPeloId: jest.fn().mockResolvedValue(agendamentoMock),
+    passeioAgendadoAdicionado: jest.fn().mockResolvedValue(agendamentoMock),
+    passeioAgendadoAtualizado: jest.fn().mockResolvedValue(agendamentoMock),
+    passeioAgendadoDeletado: jest.fn().mockResolvedValue(true),
+  },
+};

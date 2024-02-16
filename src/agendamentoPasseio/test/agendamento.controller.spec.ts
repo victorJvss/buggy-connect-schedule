1fe8e13aadf15e05de50604ea84d58a6c04df32b
joão Victor <114033813+victorJvss@users.noreply.github.com>
2024-02-16 14:29:03 -0300

    Criação do teste que deve retornar todos os passeios agendados

---
+++ src/agendamentoPasseio/test/agendamento.controller.spec.ts
@@ -0,0 +1,26 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AgendamentoPasseioService } from '../agendamento.service';
+import {
+  agendamentoMock,
+  agendamentoMockService,
+} from '../mocks/agendamento.service.mock';
+
+describe('Teste do agendamento controller', () => {
+  let agendamentoService: AgendamentoPasseioService;
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [],
+      providers: [agendamentoMockService],
+    }).compile();
+
+    agendamentoService = module.get<AgendamentoPasseioService>(
+      AgendamentoPasseioService,
+    );
+  });
+
+  it('Deve retornar todos os passeios agendados', async () => {
+    const retornarPasseioAgendado =
+      await agendamentoService.retornarTodosOsPasseiosAgendados();
+    expect(retornarPasseioAgendado).toEqual([agendamentoMock]);
+  });
+});

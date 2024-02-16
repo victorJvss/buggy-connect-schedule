192dbea1ab1c6c1b4bb6dc27457b4d3aa12c3a60
joão Victor <114033813+victorJvss@users.noreply.github.com>
2024-02-16 11:47:13 -0300

    Injeção do model Agendamento no agendamentoService

--- src/agendamentoPasseio/agendamento.service.ts
+++ src/agendamentoPasseio/agendamento.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Agendamento } from './agendamentoSchema/agendamento.schema';
+import mongoose from 'mongoose';
 
 @Injectable()
-export class agendamentoPasseioService {}
+export class agendamentoPasseioService {
+  constructor(
+    @InjectModel(Agendamento.name)
+    private Agendamento: mongoose.Model<Agendamento>,
+  ) {}
+}

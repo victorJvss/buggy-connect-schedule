0c14cde31cd6aaef96474c036f147442886bd3c6
joão Victor <114033813+victorJvss@users.noreply.github.com>
2024-02-16 11:26:31 -0300

    Criação é injeção do modulo agendamentoPasseio

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { usuarioModule } from './usuario/usuario.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config'
 import { PasseiosModule } from './passeios/passeios.module';
+import { AgendamentoPasseioModule } from './agendamentoPasseio/agendamento.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     usuarioModule,
     PasseiosModule,
+    AgendamentoPasseioModule,
     MongooseModule.forRoot(
       process.env.SERVIDOR_MONGO
     )  

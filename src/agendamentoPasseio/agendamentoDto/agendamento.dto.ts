dd6a97616bc1cc4e0b4a441773f7e87d1333fef1
joão Victor <114033813+victorJvss@users.noreply.github.com>
2024-02-16 12:03:38 -0300

    Criação do agendamentoPasseioDto

---
+++ src/agendamentoPasseio/agendamentoDto/agendamento.dto.ts
@@ -0,0 +1,18 @@
+import { IsNotEmpty } from 'class-validator';
+
+export class AgendamentoPasseioDto {
+  @IsNotEmpty({ message: 'Este campo não pode ser vazio' })
+  PasseioAgendado: string;
+  @IsNotEmpty({ message: 'Este campo não pode ser vazio' })
+  HorarioInícioPasseio: string;
+  @IsNotEmpty({ message: 'Este campo não pode ser vazio' })
+  HorarioTerminoPasseio: string;
+  @IsNotEmpty({ message: 'Este campo não pode ser vazio' })
+  QuantidadePessoas: string;
+  @IsNotEmpty({ message: 'Este campo não pode ser vazio' })
+  ValorTotalPasseio: string;
+  @IsNotEmpty({ message: 'Este campo não pode ser vazio' })
+  ProfissionalPasseio: string;
+  @IsNotEmpty({ message: 'Este campo não pode ser vazio' })
+  titularDoAgendamento: string;
+}

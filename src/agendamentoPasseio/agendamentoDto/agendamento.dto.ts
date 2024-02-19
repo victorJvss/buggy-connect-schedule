05b454eaee3c0663eadd01cd514900125ac80343
joão Victor <114033813+victorJvss@users.noreply.github.com>
2024-02-19 16:13:52 -0300

    Implementando os valores dos schemas passeioDto e agendamentoDto no swagger

--- src/agendamentoPasseio/agendamentoDto/agendamento.dto.ts
+++ src/agendamentoPasseio/agendamentoDto/agendamento.dto.ts
@@ -1,18 +1,26 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty } from 'class-validator';
 
 export class AgendamentoPasseioDto {
+  @ApiProperty()
   @IsNotEmpty({ message: 'Este campo não pode ser vazio' })
   PasseioAgendado: string;
+  @ApiProperty()
   @IsNotEmpty({ message: 'Este campo não pode ser vazio' })
   HorarioInícioPasseio: string;
+  @ApiProperty()
   @IsNotEmpty({ message: 'Este campo não pode ser vazio' })
   HorarioTerminoPasseio: string;
+  @ApiProperty()
   @IsNotEmpty({ message: 'Este campo não pode ser vazio' })
   QuantidadePessoas: number;
+  @ApiProperty()
   @IsNotEmpty({ message: 'Este campo não pode ser vazio' })
   ValorTotalPasseio: string;
+  @ApiProperty()
   @IsNotEmpty({ message: 'Este campo não pode ser vazio' })
   ProfissionalPasseio: string;
+  @ApiProperty()
   @IsNotEmpty({ message: 'Este campo não pode ser vazio' })
   titularDoAgendamento: string;
 }
--- src/passeios/passeiosDto/passeios.dto.ts
+++ src/passeios/passeiosDto/passeios.dto.ts
@@ -1,23 +1,30 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, MaxLength } from 'class-validator';
 
 export class PasseiosDto {
+  @ApiProperty()
   @IsNotEmpty({ message: 'Esse campo não pode ser vazio' })
   @MaxLength(30, { message: 'Você atingiu o maxímo de caracteres, "30"' })
   nomePasseio: string;
 
+  @ApiProperty()
   @IsNotEmpty({ message: 'Esse campo não pode ser vazio' })
   @MaxLength(500, { message: 'Você atingiu o maxímo de caracteres, "500"' })
   datalhesPasseios: string;
 
+  @ApiProperty()
   @IsNotEmpty({ message: 'Esse campo não pode ser vazio' })
   horarios: string;
 
+  @ApiProperty()
   @IsNotEmpty({ message: 'Esse campo não pode ser vazio' })
   distancia: string;
 
+  @ApiProperty()
   @IsNotEmpty({ message: 'Esse campo não pode ser vazio' })
   tempoPasseio: string;
 
+  @ApiProperty()
   @IsNotEmpty({ message: 'Esse campo não pode ser vazio' })
   valor: string;
 
